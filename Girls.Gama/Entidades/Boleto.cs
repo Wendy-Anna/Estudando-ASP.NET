08d9f047eb783789813ebf6c078028011ee3b622
Wendy-Anna <wendyufpb@gmail.com>
2021-02-03 16:51:35 -0300

    Correcoes no Boleto

--- Girls.Gama/Entidades/Boleto.cs
+++ Girls.Gama/Entidades/Boleto.cs
@@ -1,16 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Girls.Gama.Entidades
+namespace SistemaDePagamento_Aula2_GamaAcademy.Entidades
 {
     public class Boleto
+
     {
-
         private const int DiasVencimento = 15;
-        private const double Juros = 0.10;
-
-        public Boleto(string cpf, decimal valor, string descricao)
+        private const Decimal Juros = 0.10M;
+        public Boleto(string cpf,
+                      decimal valor, 
+                      string descricao)
         {
             Cpf = cpf;
             Valor = valor;
@@ -19,42 +20,36 @@ namespace Girls.Gama.Entidades
             Confirmacao = false;
         }
 
-        public Guid CodigoBarra{ get; set; }
-
-        public double Valor { get; set; }
-
+        public Guid CodigoBarra { get; set; }
+        public decimal Valor { get; set; }
         public DateTime DataEmissao { get; set; }
-
         public DateTime DataVencimento { get; set; }
-
         public DateTime DataPagamento { get; set; }
-
-        public string Cpf { get; set; }
-
-        public string Descricao { get; set; }
-
         public bool Confirmacao { get; set; }
+        public string Cpf { get; set; }
+        public string Descricao { get; set; }
 
         public void GerarBoleto()
         {
-
             CodigoBarra = Guid.NewGuid();
             DataVencimento = DataEmissao.AddDays(DiasVencimento);
         }
+        
+        public bool BoletoEstaPago()
+        {
+            return Confirmacao;
+        }
 
- 
-        public void  CalcularJuros()
+        public void calcularJuros()
         {
             var taxa = Valor * Juros;
             Valor += taxa;
         }
-
 
         public void Pagar()
         {
             DataPagamento = DateTime.Now;
             Confirmacao = true;
         }
-
     }
 }
